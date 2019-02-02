90567c3ffd8034a35c61fa7e1c8d06d8504dbd65
lunargorge <lunargorge@gmail.com>
2019-02-02 17:09:41 +0100

    add count endpoiint and CORS

--- db.js
+++ db.js
@@ -10,6 +10,7 @@ var summary = require('./endpoints/mock/summary');
 
 // custom route
 var routeSummary = require('./endpoints/route/summary');
+var routeCommentsCount = require('./endpoints/route/comments-count');
 
 // prepare data object
 var dataPrepare = function() {
@@ -34,10 +35,20 @@ var middlewares = jsonServer.defaults({noCors: true});
 
 var db = router.db;
 
+// server.use(middlewares);
+server.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+   });
+
 // GET: /summary
 // GET: /summary/?userId=1
 server.get('/summary', routeSummary.get(db));
 server.get('/summary/:id', routeSummary.getById(db));
+server.get('/comments/count', routeCommentsCount.count(db));
 
 server.use(jsonServer.rewriter({
     "/posts/:postId/comments": "/comments/?postId=:postId",
